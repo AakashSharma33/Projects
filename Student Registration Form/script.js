60775ac74a47bc5a0b0626af5b723b460558bb84
AakashSharma33 <134127328+AakashSharma33@users.noreply.github.com>
2023-10-29 08:48:23 +0000

    Project

---
+++ Student Registration Form/script.js
@@ -0,0 +1,6 @@
+body = document.querySelector('body')
+response = document.getElementsByClassName('response')
+document.getElementById('submit-button').addEventListener('click' , ()=>{
+    alert('Successful Registered')
+})
+
